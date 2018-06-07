a59ac851f67aaa3e20248006cf5ef6381edb57f1
Dmitrii <dimana1507@gmail.com>
2018-06-07 23:46:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ txtInput.addEventListener('input', output);
 function output(){
     outputBlock.innerHTML = txtInput.value;
 }
-// Изменить Font Family в виводе
+// Изменить Font Family в выводе
 var fontFam = document.getElementById('font-family');
 fontFam.addEventListener('change', changeFontFam);
 
@@ -16,7 +16,7 @@ function changeFontFam(){
     var selectState = fontFam.value;
     outputBlock.style.fontFamily = `'` + selectState + `'`;
 }
-//Изменить Font стиль в виводе
+//Изменить Font стиль в выводе
 var fontStyle = document.getElementById('font-style');
 fontStyle.addEventListener('change', changeFontStyle);
 
@@ -24,7 +24,7 @@ function changeFontStyle(){
     var selectState = fontStyle.value;
     outputBlock.style.fontStyle = selectState;
 }
-//Изменить цвет шрифта в виводе
+//Изменить цвет шрифта в выводе
 var fontColor = document.getElementById('fontColor');
 fontColor.addEventListener('change', changeFontColor);
 
@@ -33,7 +33,7 @@ function changeFontColor(){
     outputBlock.style.color = selectState;
 }
 
-// Изменить размер шрифта в виводе
+// Изменить размер шрифта в выводе
 var fontSize = document.getElementById('fontSize');
 fontSize.addEventListener('change', changeFontSize);
 fontSize.addEventListener('mousemove', changeFontSize);
@@ -42,7 +42,7 @@ function changeFontSize(){
     var selectState = fontSize.value;
     outputBlock.style.fontSize = selectState + 'px';
 }
-// Изменить ширину шрифта в виводе
+// Изменить ширину шрифта в выводе
 var fontWeight = document.getElementById('fontWeight');
 fontWeight.addEventListener('change', changeFontWeight);
 fontWeight.addEventListener('mousemove', changeFontWeight);
